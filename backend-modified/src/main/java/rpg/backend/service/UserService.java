54a72412f7732b47541a2a907e88eacd61e9643e
satyam-721 <satyamsagar305@gmail.com>
2026-09-13 08:09:51 +0100

    neon migration

--- backend-modified/src/main/java/rpg/backend/service/UserService.java
+++ backend-modified/src/main/java/rpg/backend/service/UserService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import rpg.backend.dto.UserRepository;
+import rpg.backend.model.Profile;
 import rpg.backend.model.User;
+import rpg.backend.repository.ProfileRepository;
 
 @Service
 public class UserService {
@@ -12,10 +14,40 @@ public class UserService {
     @Autowired
     UserRepository repo;
 
+    @Autowired
+    ProfileRepository profileRepository;
+
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-    public User saveUser(User user){
+    public User saveUser(User user) {
+
         user.setPassword(encoder.encode(user.getPassword()));
-        return repo.save(user);
+
+        user = repo.save(user);
+
+        Profile profile = new Profile();
+
+        profile.setName(user.getUsername());
+        profile.setCallsign("PLAYER");
+        profile.setLevel(1);
+        profile.setXp(0);
+        profile.setScore(0);
+        profile.setTickets(0);
+        profile.setUnspentSkillPoints(0);
+
+        profile.setHp(100);
+        profile.setMaxHp(100);
+        profile.setEnergy(100);
+        profile.setMaxEnergy(100);
+
+        profile.setStreak(0);
+        profile.setStreakShields(0);
+        profile.setAvatar("🕹️");
+
+        profile.setUser(user);
+
+        profileRepository.save(profile);
+
+        return user;
     }
 }
